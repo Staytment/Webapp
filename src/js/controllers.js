e0400e7e7807b2f61e9bcdd7b2477b09029936f0
Soeren Wegener <wegener92@gmail.com>
2014-06-20 03:35:27 +0200

    Simple marker on map.

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -6,3 +6,32 @@ staytment.controller('Navigation', function($scope) {
     {title: 'Foobar'}
   ];
 });
+
+staytment.controller('Map', ['$scope', '$location', function($scope, $location) {
+  angular.extend($scope, {
+    center: {
+      lat: 51.3,
+      lng: 9.5,
+      zoom: 12
+    },
+    markers: {
+      myMarker: {
+        lat: 51.325,
+        lng: 9.469,
+        message: 'DAFUQ!',
+        focus: false,
+        draggable: false
+      },
+      my2Marker: {
+        lat: 51.275,
+        lng: 9.439,
+        message: 'YOLO!',
+        focus: false,
+        draggable: false
+      }
+    }
+  });
+  $scope.$on('centerUrlHash', function(event, centerHash) {
+    $location.search({ c: centerHash });
+  });
+}]);
