03f3c5674b1a7e03848550895a23a4224e046b39
Soeren Wegener <wegener92@gmail.com>
2014-06-22 13:52:55 +0200

    Added map events.
    Fetch new data when the map is moved or zoomed.

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,3 @@
 angular.module('config', [])
   .constant('DOMAIN_API','http://localhost:5000');
+  //.constant('DOMAIN_API','http://api.staytment.com');
--- src/js/controllers.js
+++ src/js/controllers.js
@@ -8,40 +8,16 @@ staytment.controller('Navigation', function ($scope) {
 });
 
 staytment.controller('Map', ['$scope', '$http', '$location', 'DOMAIN_API', function ($scope, $http, $location, DOMAIN_API) {
-  var markers = {
-    myMarker: {
-      lat: 51.325,
-      lng: 9.469,
-      message: 'DAFUQ!',
-      focus: true,
-      draggable: false,
-      autoPan: false
-    },
-    my2Marker: {
-      lat: 51.275,
-      lng: 9.439,
-      message: 'YOLO!',
-      focus: true,
-      draggable: false,
-      autoPan: false
-    }
-  };
-  angular.extend($scope, {
-    center: {
-      lat: 51.3,
-      lng: 9.5,
-      zoom: 12
-    },
-    markers: markers
-  });
-  setInterval(function () {
+  function fetchPosts(event) {
+    var lat = $scope.center.lat;
+    var long = $scope.center.lng;
     $http.get(DOMAIN_API + '/posts').success(function (data) {
       var markers = {};
       for (var i = 0; i < data.length; i++) {
         markers[data[i]._id] = {
           lat: data[i].lat,
           lng: data[i].long,
-          message: data[i].message,
+          message: lat + ',' + long, // data[i].message,
           focus: true,
           draggable: false,
           autoPan: false
@@ -51,8 +27,27 @@ staytment.controller('Map', ['$scope', '$http', '$location', 'DOMAIN_API', funct
         markers: markers
       });
     });
-  }, 3000);
+  }
+
+  angular.extend($scope, {
+    center: {
+      lat: 51.3,
+      lng: 9.5,
+      zoom: 12
+    },
+    events: {
+      map: {
+        enable: ['zoomend', 'moveend'],
+        logic: 'emit'
+      }
+    },
+    markers: {}
+  });
+
   $scope.$on('centerUrlHash', function (event, centerHash) {
     $location.search({ c: centerHash });
   });
+
+  $scope.$on('leafletDirectiveMap.zoomend', fetchPosts);
+  $scope.$on('leafletDirectiveMap.moveend', fetchPosts);
 }]);
