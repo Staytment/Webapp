f64d3bb5a3871ff600cb11a4e51154e2eb2254da
Sören Wegener <wegener92@gmail.com>
2014-08-31 23:41:59 +0200

    Implemented simple leaflet example.

--- src/js/map.js
+++ src/js/map.js
@@ -9,3 +9,9 @@ L.Map = L.Map.extend({
     });
   }
 });
+
+var map = L.map('map').setView([50.5, 9.1], 8);
+L.tileLayer('http://{s}.tiles.mapbox.com/v3/swegener.jckkpg0b/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18
+}).addTo(map);
